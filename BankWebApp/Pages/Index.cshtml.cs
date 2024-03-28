23ad0d60410fcf8e953853794c9e6a4902a08adc
WILGROSS <william-gross@hotmail.com>
2024-03-28 11:19:14 +0100

    Added statistics by country

--- BankWebApp/Pages/Index.cshtml.cs
+++ BankWebApp/Pages/Index.cshtml.cs
@@ -18,11 +18,38 @@ namespace BankWebApp.Pages
 
         public void OnGet()
         {
-            _landingPageViewModel = new()
+            var countries = _context.Customers
+                                    .Select(c => c.Country)
+                                    .Distinct()
+                                    .ToList();
+
+            _landingPageViewModel = new LandingPageViewModel
             {
                 TotalCustomers = _context.Customers.Count(),
                 TotalAccounts = _context.Accounts.Count(),
-                TotalBalance = _context.Accounts.Sum(a => a.Balance)
+                TotalBalance = _context.Accounts.Sum(a => a.Balance),
+
+                CountriesInfo = countries.Select(x => new CountryInfoViewModel
+                {
+                    Country = x,
+                    TotalCustomers = _context.Customers.Count(c => c.Country == x),
+                    TotalAccounts = _context.Dispositions
+                                            .Where(d => d.Customer.Country == x)
+                                            .Select(d => d.AccountId)
+                                            .Distinct()
+                                            .Count(),
+                    TotalBalance = _context.Dispositions
+                                           .Where(d => d.Customer.Country == x)
+                                           .Select(d => d.Account)
+                                           .Sum(a => a.Balance),
+                    TopBalances = _context.Dispositions
+                                          .Where(d => d.Customer.Country == x)
+                                          .Select(d => d.Account)
+                                          .OrderByDescending(a => a.Balance)
+                                          .Take(10)
+                                          .Select(a => a.Balance)
+                                          .ToList()
+                }).ToList()
             };
         }
     }
--- BankWebApp/ViewModels/LandingPageViewModel.cs
+++ BankWebApp/ViewModels/LandingPageViewModel.cs
@@ -2,8 +2,19 @@
 {
     public class LandingPageViewModel
     {
+        public List<CountryInfoViewModel> CountriesInfo { get; set; }
         public int TotalCustomers { get; set; }
         public int TotalAccounts { get; set; }
         public decimal TotalBalance { get; set; }
     }
+    public class CountryInfoViewModel
+    {
+        public string Country { get; set; }
+        public string CountryCode { get; set; }
+        public int TotalCustomers { get; set; }
+        public int TotalAccounts { get; set; }
+        public decimal TotalBalance { get; set; }
+        public List<decimal> TopBalances { get; set; }
+    }
+
 }
