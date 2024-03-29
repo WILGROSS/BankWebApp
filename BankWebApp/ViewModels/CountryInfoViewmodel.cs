39add0bfd7e691375bea13508a81ca6da79dd914
Fittgross <william-gross@hotmail.com>
2024-03-29 12:27:31 +0100

    Moved CountryInfoViewmodel to it's own cs file

---
+++ BankWebApp/ViewModels/CountryInfoViewmodel.cs
@@ -0,0 +1,12 @@
+﻿namespace BankWebApp.ViewModels
+{
+    public class CountryInfoViewModel
+    {
+        public string Country { get; set; }
+        public string CountryCode { get; set; }
+        public int TotalCustomers { get; set; }
+        public int TotalAccounts { get; set; }
+        public decimal TotalBalance { get; set; }
+        public List<decimal> TopBalances { get; set; }
+    }
+}
