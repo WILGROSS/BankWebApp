f6affe615c563b4a98a13e2e57e208bafa216115
Fittgross <william-gross@hotmail.com>
2024-04-01 15:26:07 +0200

    Added address and national ID to customers table

--- BankWebApp/ViewModels/CustomerViewmodel.cs
+++ BankWebApp/ViewModels/CustomerViewmodel.cs
@@ -3,10 +3,12 @@
     public class CustomerViewmodel
     {
         public int CustomerId { get; set; }
+        public string NationalId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Country { get; set; }
         public string City { get; set; }
+        public string Address { get; set; }
 
     }
 }
