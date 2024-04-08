b8821d017c7896b6089511bf4953a0810e27da42
Fittgross <william-gross@hotmail.com>
2024-04-09 00:03:27 +0200

    More or less finished ViewSingleCustomer

--- Services/ViewSingleCustomerService.cs
+++ Services/ViewSingleCustomerService.cs
@@ -27,7 +27,7 @@ namespace Services
 			{
 				account.LatestTransactions = account.Transactions
 					.OrderByDescending(t => t.TransactionId)
-					.Take(8)
+					.Take(10)
 					.ToList();
 
 				viewModel.TotalBalance += account.Balance;
