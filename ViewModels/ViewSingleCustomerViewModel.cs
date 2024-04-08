6ff1eea992cb96c0a1c2658364dc7616d1a82c52
Fittgross <william-gross@hotmail.com>
2024-04-08 19:21:17 +0200

    Finished rough html structure for ViewSingleCustomer

--- ViewModels/ViewSingleCustomerViewModel.cs
+++ ViewModels/ViewSingleCustomerViewModel.cs
@@ -7,14 +7,14 @@
 		public string Gender { get; set; }
 		public string GivenName { get; set; }
 		public string SurName { get; set; }
-		public DateOnly DateOfBirth { get; set; }
+		public DateOnly Birthday { get; set; }
 		public string Country { get; set; }
 		public string CountryCode { get; set; }
 		public string City { get; set; }
-		public string Address { get; set; }
+		public string StreetAddress { get; set; }
 		public string ZipCode { get; set; }
-		public string PhoneCountryCode { get; set; }
-		public string PhoneNumber { get; set; }
+		public string TelephoneCountryCode { get; set; }
+		public string TelephoneNumber { get; set; }
 		public string EmailAddress { get; set; }
 		public decimal TotalBalance { get; set; }
 		public List<AccountViewModel> Accounts { get; set; }
