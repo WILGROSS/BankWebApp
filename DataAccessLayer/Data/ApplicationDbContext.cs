5eda0cbca9f8bb8a01e5e0231ace055348343353
Fittgross <william-gross@hotmail.com>
2024-04-11 23:37:41 +0200

    Renamed database and fixed migrations

--- DataAccessLayer/Data/ApplicationDbContext.cs
+++ DataAccessLayer/Data/ApplicationDbContext.cs
@@ -5,162 +5,162 @@ namespace DataAccessLayer.Data;
 
 public partial class ApplicationDbContext : IdentityDbContext
 {
-    public ApplicationDbContext()
-    {
-    }
+	public ApplicationDbContext()
+	{
+	}
 
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
-    {
-    }
+	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+		: base(options)
+	{
+	}
 
-    public virtual DbSet<Account> Accounts { get; set; }
+	public virtual DbSet<Account> Accounts { get; set; }
 
-    public virtual DbSet<Card> Cards { get; set; }
+	public virtual DbSet<Card> Cards { get; set; }
 
-    public virtual DbSet<Customer> Customers { get; set; }
+	public virtual DbSet<Customer> Customers { get; set; }
 
-    public virtual DbSet<Disposition> Dispositions { get; set; }
+	public virtual DbSet<Disposition> Dispositions { get; set; }
 
-    public virtual DbSet<Loan> Loans { get; set; }
+	public virtual DbSet<Loan> Loans { get; set; }
 
-    public virtual DbSet<PermenentOrder> PermenentOrders { get; set; }
+	public virtual DbSet<PermenentOrder> PermenentOrders { get; set; }
 
-    public virtual DbSet<Transaction> Transactions { get; set; }
+	public virtual DbSet<Transaction> Transactions { get; set; }
 
-    public virtual DbSet<User> Users { get; set; }
+	public virtual DbSet<User> Users { get; set; }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
-        modelBuilder.Entity<Account>(entity =>
-        {
-            entity.HasKey(e => e.AccountId).HasName("PK_account");
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		base.OnModelCreating(modelBuilder);
+		modelBuilder.Entity<Account>(entity =>
+		{
+			entity.HasKey(e => e.AccountId).HasName("PK_account");
 
-            entity.Property(e => e.Balance).HasColumnType("decimal(13, 2)");
-            entity.Property(e => e.Frequency).HasMaxLength(50);
-        });
+			entity.Property(e => e.Balance).HasColumnType("decimal(13, 2)");
+			entity.Property(e => e.Frequency).HasMaxLength(50);
+		});
 
-        modelBuilder.Entity<Card>(entity =>
-        {
-            entity.Property(e => e.Ccnumber)
-                .HasMaxLength(50)
-                .HasColumnName("CCNumber");
-            entity.Property(e => e.Cctype)
-                .HasMaxLength(50)
-                .HasColumnName("CCType");
-            entity.Property(e => e.Cvv2)
-                .HasMaxLength(10)
-                .HasColumnName("CVV2");
-            entity.Property(e => e.Type).HasMaxLength(50);
+		modelBuilder.Entity<Card>(entity =>
+		{
+			entity.Property(e => e.Ccnumber)
+				.HasMaxLength(50)
+				.HasColumnName("CCNumber");
+			entity.Property(e => e.Cctype)
+				.HasMaxLength(50)
+				.HasColumnName("CCType");
+			entity.Property(e => e.Cvv2)
+				.HasMaxLength(10)
+				.HasColumnName("CVV2");
+			entity.Property(e => e.Type).HasMaxLength(50);
 
-            entity.HasOne(d => d.Disposition).WithMany(p => p.Cards)
-                .HasForeignKey(d => d.DispositionId)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK_Cards_Dispositions");
-        });
+			entity.HasOne(d => d.Disposition).WithMany(p => p.Cards)
+				.HasForeignKey(d => d.DispositionId)
+				.OnDelete(DeleteBehavior.ClientSetNull)
+				.HasConstraintName("FK_Cards_Dispositions");
+		});
 
-        modelBuilder.Entity<Customer>(entity =>
-        {
-            entity.Property(e => e.City).HasMaxLength(100);
-            entity.Property(e => e.Country).HasMaxLength(100);
-            entity.Property(e => e.CountryCode).HasMaxLength(2);
-            entity.Property(e => e.Emailaddress).HasMaxLength(100);
-            entity.Property(e => e.Gender).HasMaxLength(6);
-            entity.Property(e => e.Givenname).HasMaxLength(100);
-            entity.Property(e => e.NationalId).HasMaxLength(20);
-            entity.Property(e => e.Streetaddress).HasMaxLength(100);
-            entity.Property(e => e.Surname).HasMaxLength(100);
-            entity.Property(e => e.Telephonecountrycode).HasMaxLength(10);
-            entity.Property(e => e.Telephonenumber).HasMaxLength(25);
-            entity.Property(e => e.Zipcode).HasMaxLength(15);
-        });
+		modelBuilder.Entity<Customer>(entity =>
+		{
+			entity.Property(e => e.City).HasMaxLength(100);
+			entity.Property(e => e.Country).HasMaxLength(100);
+			entity.Property(e => e.CountryCode).HasMaxLength(2);
+			entity.Property(e => e.Emailaddress).HasMaxLength(100);
+			entity.Property(e => e.Gender).HasMaxLength(6);
+			entity.Property(e => e.Givenname).HasMaxLength(100);
+			entity.Property(e => e.NationalId).HasMaxLength(20);
+			entity.Property(e => e.Streetaddress).HasMaxLength(100);
+			entity.Property(e => e.Surname).HasMaxLength(100);
+			entity.Property(e => e.Telephonecountrycode).HasMaxLength(10);
+			entity.Property(e => e.Telephonenumber).HasMaxLength(25);
+			entity.Property(e => e.Zipcode).HasMaxLength(15);
+		});
 
-        modelBuilder.Entity<Disposition>(entity =>
-        {
-            entity.HasKey(e => e.DispositionId).HasName("PK_disposition");
+		modelBuilder.Entity<Disposition>(entity =>
+		{
+			entity.HasKey(e => e.DispositionId).HasName("PK_disposition");
 
-            entity.Property(e => e.Type).HasMaxLength(50);
+			entity.Property(e => e.Type).HasMaxLength(50);
 
-            entity.HasOne(d => d.Account).WithMany(p => p.Dispositions)
-                .HasForeignKey(d => d.AccountId)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK_Dispositions_Accounts");
+			entity.HasOne(d => d.Account).WithMany(p => p.Dispositions)
+				.HasForeignKey(d => d.AccountId)
+				.OnDelete(DeleteBehavior.ClientSetNull)
+				.HasConstraintName("FK_Dispositions_Accounts");
 
-            entity.HasOne(d => d.Customer).WithMany(p => p.Dispositions)
-                .HasForeignKey(d => d.CustomerId)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK_Dispositions_Customers");
-        });
+			entity.HasOne(d => d.Customer).WithMany(p => p.Dispositions)
+				.HasForeignKey(d => d.CustomerId)
+				.OnDelete(DeleteBehavior.ClientSetNull)
+				.HasConstraintName("FK_Dispositions_Customers");
+		});
 
-        modelBuilder.Entity<Loan>(entity =>
-        {
-            entity.HasKey(e => e.LoanId).HasName("PK_loan");
+		modelBuilder.Entity<Loan>(entity =>
+		{
+			entity.HasKey(e => e.LoanId).HasName("PK_loan");
 
-            entity.Property(e => e.Amount).HasColumnType("decimal(13, 2)");
-            entity.Property(e => e.Payments).HasColumnType("decimal(13, 2)");
-            entity.Property(e => e.Status).HasMaxLength(50);
+			entity.Property(e => e.Amount).HasColumnType("decimal(13, 2)");
+			entity.Property(e => e.Payments).HasColumnType("decimal(13, 2)");
+			entity.Property(e => e.Status).HasMaxLength(50);
 
-            entity.HasOne(d => d.Account).WithMany(p => p.Loans)
-                .HasForeignKey(d => d.AccountId)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK_Loans_Accounts");
-        });
+			entity.HasOne(d => d.Account).WithMany(p => p.Loans)
+				.HasForeignKey(d => d.AccountId)
+				.OnDelete(DeleteBehavior.ClientSetNull)
+				.HasConstraintName("FK_Loans_Accounts");
+		});
 
-        modelBuilder.Entity<PermenentOrder>(entity =>
-        {
-            entity.HasKey(e => e.OrderId);
+		modelBuilder.Entity<PermenentOrder>(entity =>
+		{
+			entity.HasKey(e => e.OrderId);
 
-            entity.ToTable("PermenentOrder");
+			entity.ToTable("PermenentOrder");
 
-            entity.Property(e => e.AccountTo).HasMaxLength(50);
-            entity.Property(e => e.Amount).HasColumnType("decimal(13, 2)");
-            entity.Property(e => e.BankTo).HasMaxLength(50);
-            entity.Property(e => e.Symbol).HasMaxLength(50);
+			entity.Property(e => e.AccountTo).HasMaxLength(50);
+			entity.Property(e => e.Amount).HasColumnType("decimal(13, 2)");
+			entity.Property(e => e.BankTo).HasMaxLength(50);
+			entity.Property(e => e.Symbol).HasMaxLength(50);
 
-            entity.HasOne(d => d.Account).WithMany(p => p.PermenentOrders)
-                .HasForeignKey(d => d.AccountId)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK_PermenentOrder_Accounts");
-        });
+			entity.HasOne(d => d.Account).WithMany(p => p.PermenentOrders)
+				.HasForeignKey(d => d.AccountId)
+				.OnDelete(DeleteBehavior.ClientSetNull)
+				.HasConstraintName("FK_PermenentOrder_Accounts");
+		});
 
-        modelBuilder.Entity<Transaction>(entity =>
-        {
-            entity.HasKey(e => e.TransactionId).HasName("PK_trans2");
+		modelBuilder.Entity<Transaction>(entity =>
+		{
+			entity.HasKey(e => e.TransactionId).HasName("PK_trans2");
 
-            entity.HasIndex(e => e.AccountId, "IX_Transactions_AccountId");
+			entity.HasIndex(e => e.AccountId, "IX_Transactions_AccountId");
 
-            entity.Property(e => e.Account).HasMaxLength(50);
-            entity.Property(e => e.Amount).HasColumnType("decimal(13, 2)");
-            entity.Property(e => e.Balance).HasColumnType("decimal(13, 2)");
-            entity.Property(e => e.Bank).HasMaxLength(50);
-            entity.Property(e => e.Operation).HasMaxLength(50);
-            entity.Property(e => e.Symbol).HasMaxLength(50);
-            entity.Property(e => e.Type).HasMaxLength(50);
+			entity.Property(e => e.Account).HasMaxLength(50);
+			entity.Property(e => e.Amount).HasColumnType("decimal(13, 2)");
+			entity.Property(e => e.Balance).HasColumnType("decimal(13, 2)");
+			entity.Property(e => e.Bank).HasMaxLength(50);
+			entity.Property(e => e.Operation).HasMaxLength(50);
+			entity.Property(e => e.Symbol).HasMaxLength(50);
+			entity.Property(e => e.Type).HasMaxLength(50);
 
-            entity.HasOne(d => d.AccountNavigation).WithMany(p => p.Transactions)
-                .HasForeignKey(d => d.AccountId)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK_Transactions_Accounts");
-        });
+			entity.HasOne(d => d.AccountNavigation).WithMany(p => p.Transactions)
+				.HasForeignKey(d => d.AccountId)
+				.OnDelete(DeleteBehavior.ClientSetNull)
+				.HasConstraintName("FK_Transactions_Accounts");
+		});
 
-        modelBuilder.Entity<User>(entity =>
-        {
-            entity.HasKey(e => e.UserId).HasName("PK_User_UserID");
+		modelBuilder.Entity<User>(entity =>
+		{
+			entity.HasKey(e => e.UserId).HasName("PK_User_UserID");
 
-            entity.ToTable("User");
+			entity.ToTable("User");
 
-            entity.Property(e => e.UserId).HasColumnName("UserID");
-            entity.Property(e => e.FirstName).HasMaxLength(40);
-            entity.Property(e => e.LastName).HasMaxLength(40);
-            entity.Property(e => e.LoginName).HasMaxLength(40);
-            entity.Property(e => e.PasswordHash)
-                .HasMaxLength(64)
-                .IsFixedLength();
-        });
+			entity.Property(e => e.UserId).HasColumnName("UserID");
+			entity.Property(e => e.FirstName).HasMaxLength(40);
+			entity.Property(e => e.LastName).HasMaxLength(40);
+			entity.Property(e => e.LoginName).HasMaxLength(40);
+			entity.Property(e => e.PasswordHash)
+				.HasMaxLength(64)
+				.IsFixedLength();
+		});
 
-        OnModelCreatingPartial(modelBuilder);
-    }
+		OnModelCreatingPartial(modelBuilder);
+	}
 
-    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+	partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
--- DataAccessLayer/Data/Migrations/20240411213230_Customer isActive to isDeleted.Designer.cs
+++ DataAccessLayer/Data/Migrations/20240411213230_Customer isActive to isDeleted.Designer.cs
@@ -4,6 +4,7 @@ using DataAccessLayer.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BankWebApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240411213230_Customer isActive to isDeleted")]
+    partial class CustomerisActivetoisDeleted
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
