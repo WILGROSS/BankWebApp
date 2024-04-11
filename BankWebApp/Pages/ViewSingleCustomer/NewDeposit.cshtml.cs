370fbeb7aa7726dc00fdd76f78cfb2324df49a57
Fittgross <william-gross@hotmail.com>
2024-04-11 14:13:15 +0200

    started working on a Deposit page

---
+++ BankWebApp/Pages/ViewSingleCustomer/NewDeposit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BankWebApp.Pages.ViewSingleCustomer
+{
+    public class NewDepositModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
