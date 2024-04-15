dfb90125374a42d5f794ecf5f8497126f50ed141
Fittgross <william-gross@hotmail.com>
2024-04-16 01:22:55 +0200

    Switched out some connection strings to match latest Azure db

--- DataAccessLayer/Data/DataInitializer.cs
+++ DataAccessLayer/Data/DataInitializer.cs
@@ -3,60 +3,60 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DataAccessLayer.Data
 {
-    public class DataInitializer
-    {
-        private readonly ApplicationDbContext _dbContext;
-        private readonly UserManager<IdentityUser> _userManager;
+	public class DataInitializer
+	{
+		private readonly ApplicationDbContext _dbContext;
+		private readonly UserManager<IdentityUser> _userManager;
 
-        public DataInitializer(ApplicationDbContext dbContext, UserManager<IdentityUser> userManager)
-        {
-            _dbContext = dbContext;
-            _userManager = userManager;
-        }
-        public void SeedData()
-        {
-            _dbContext.Database.Migrate();
-            SeedRoles();
-            SeedUsers();
-        }
+		public DataInitializer(ApplicationDbContext dbContext, UserManager<IdentityUser> userManager)
+		{
+			_dbContext = dbContext;
+			_userManager = userManager;
+		}
+		public void SeedData()
+		{
+			SeedRoles();
+			SeedUsers();
+			_dbContext.Database.Migrate();
+		}
 
-        // Här finns möjlighet att uppdatera dina användares loginuppgifter
-        private void SeedUsers()
-        {
-            AddUserIfNotExists("richard.chalk@systementor.se", "Hejsan123#", new string[] { "Admin" });
-            AddUserIfNotExists("richard.chalk@cashier.systementor.se", "Hejsan123#", new string[] { "Cashier" });
-            AddUserIfNotExists("william@gross.nu", "Hejsan123#", new string[] { "Cashier", "Admin" });
-        }
+		// Här finns möjlighet att uppdatera dina användares loginuppgifter
+		private void SeedUsers()
+		{
+			AddUserIfNotExists("richard.chalk@systementor.se", "Hejsan123#", new string[] { "Admin" });
+			AddUserIfNotExists("richard.chalk@cashier.systementor.se", "Hejsan123#", new string[] { "Cashier" });
+			AddUserIfNotExists("william@gross.nu", "Hejsan123#", new string[] { "Cashier", "Admin" });
+		}
 
-        // Här finns möjlighet att uppdatera dina användares roller
-        private void SeedRoles()
-        {
-            AddRoleIfNotExisting("Admin");
-            AddRoleIfNotExisting("Cashier");
-        }
+		// Här finns möjlighet att uppdatera dina användares roller
+		private void SeedRoles()
+		{
+			AddRoleIfNotExisting("Admin");
+			AddRoleIfNotExisting("Cashier");
+		}
 
-        private void AddRoleIfNotExisting(string roleName)
-        {
-            var role = _dbContext.Roles.FirstOrDefault(r => r.Name == roleName);
-            if (role == null)
-            {
-                _dbContext.Roles.Add(new IdentityRole { Name = roleName, NormalizedName = roleName });
-                _dbContext.SaveChanges();
-            }
-        }
+		private void AddRoleIfNotExisting(string roleName)
+		{
+			var role = _dbContext.Roles.FirstOrDefault(r => r.Name == roleName);
+			if (role == null)
+			{
+				_dbContext.Roles.Add(new IdentityRole { Name = roleName, NormalizedName = roleName });
+				_dbContext.SaveChanges();
+			}
+		}
 
-        private void AddUserIfNotExists(string userName, string password, string[] roles)
-        {
-            if (_userManager.FindByEmailAsync(userName).Result != null) return;
+		private void AddUserIfNotExists(string userName, string password, string[] roles)
+		{
+			if (_userManager.FindByEmailAsync(userName).Result != null) return;
 
-            var user = new IdentityUser
-            {
-                UserName = userName,
-                Email = userName,
-                EmailConfirmed = true
-            };
-            _userManager.CreateAsync(user, password).Wait();
-            _userManager.AddToRolesAsync(user, roles).Wait();
-        }
-    }
+			var user = new IdentityUser
+			{
+				UserName = userName,
+				Email = userName,
+				EmailConfirmed = true
+			};
+			_userManager.CreateAsync(user, password).Wait();
+			_userManager.AddToRolesAsync(user, roles).Wait();
+		}
+	}
 }
